a91995b1a8ff233e4aa55656b42b202f4de87ed2
vilichtSarina <sarina.lusti@noseryoung.ch>
2023-03-09 15:46:53 +0100

    adjust caption

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -8,23 +8,20 @@ export default function HomePage() {
 
   return (
     <Box className={"box"}>
-      <h1 className={"our"}>CAT EAT</h1>
-      <h1 className={"space"}>SOLID MILK</h1>
+      <h1 className={"space"}>ARSON</h1>
       <div className="textSign">
-        <p>PROJECT FOR UK223 </p>
+        <p>and other crimes we support </p>
         <hr />
       </div>
 
       <button className={"loginButton"} onClick={() => navigate("/login")}>LOGIN</button>
-      <center>
-        <iframe
-          src="https://i.redd.it/g61r1687wkd91.gif"
-          frameBorder="0"
-          width="100%"
-          height="100%"
-          title="logo"
-        ></iframe>
-      </center>
+      <iframe
+        src="https://i.redd.it/g61r1687wkd91.gif"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        title="logo"
+      ></iframe>
       <img src={logo} className="App-logo img" alt="logo" />
     </Box>
   );
